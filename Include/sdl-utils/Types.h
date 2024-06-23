40e2040f14bf5d6069b74e3971e10322b81bc162
splatoon1enjoyer <administrator@sdlfoundation.com>
2024-06-23 15:29:37 +0300

    Add the rest of the types in the conditional compilation

--- Include/sdl-utils/Types.h
+++ Include/sdl-utils/Types.h
@@ -11,13 +11,11 @@ typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
-typedef __uint128_t u128;
 
 typedef int8_t s8;
 typedef int16_t s16;
 typedef int32_t s32;
 typedef int64_t s64;
-typedef __int128_t s128;
 
 typedef float f32;
 typedef double f64;
@@ -29,13 +27,11 @@ typedef volatile u8 vu8;
 typedef volatile u16 vu16;
 typedef volatile u32 vu32;
 typedef volatile u64 vu64;
-typedef volatile u128 vu128;
 
 typedef volatile s8 vs8;
 typedef volatile s16 vs16;
 typedef volatile s32 vs32;
 typedef volatile s64 vs64;
-typedef volatile s128 vs128;
 
 typedef volatile f32 vf32;
 typedef volatile f64 vf64;
@@ -51,8 +47,13 @@ typedef unsigned long ulong;
 
 #include <quadmath.h>
 
+typedef __uint128_t u128;
+typedef __int128_t s128;
 typedef __float128 f128;
+
 typedef volatile f128 vf128;
+typedef volatile u128 vu128;
+typedef volatile s128 vs128;
 
 #endif
 
